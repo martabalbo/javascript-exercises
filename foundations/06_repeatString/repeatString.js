0c6433f1e242bdc59a7459a935c2be57c3beca29
martabalbo <marta99.balbo@gmail.com>
2026-05-14 13:03:28 +0200

    Write function to repeat strings

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let concat_string = "";
+    for (let i=0; i<num; i++) {
+        concat_string += string;;
+    }
+    return concat_string;
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
