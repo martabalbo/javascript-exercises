1b941c188ea1d0e53b852eb46e53795b66db1079
martabalbo <marta99.balbo@gmail.com>
2026-05-15 13:31:01 +0200

    Write function reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let arr = inputString.split("");
+    console.log(arr)
+    let reversedArr = [];
+    for(let i=0; i<(inputString.length); i++) {
+        let letter = arr.pop();
+        console.log(letter);
+        reversedArr[i] = letter;
+        console.log(reversedArr)
+    }
+    return reversedArr.join("");
 };
+
+a = reverseString("ciao a tutti");
+console.log(a);
 
 // Do not edit below this line
 module.exports = reverseString;
