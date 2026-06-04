46dd899c9267dad0b3fc39fb3ba1ec5ad7c24665
martabalbo <marta99.balbo@gmail.com>
2026-06-04 19:03:40 +0200

    Add fibonacci function

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index<0) {
+        return 'OOPS';
+    }
+    let arr = [0, 1];
+    for (let i=0; i<index; i++) {
+    arr.push(arr.at(-1) + arr.at(-2));
+    };
+    return arr[index];
 
 };
 
