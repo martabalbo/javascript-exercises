8b14ac8ebeba5f4e217ecaf0c8457bac4ff865ba
martabalbo <marta99.balbo@gmail.com>
2026-06-04 18:51:40 +0200

    Add palindromes function

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    const arr = input
+                .toLowerCase()
+                .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+                .replace(/ /g, "")
+                .split('');
+    //console.log(arr);
+    let newArr = Array.from(arr).toReversed();
+    //console.log(newArr)
+    return arr.toString() == newArr.toString();
 };
+
+//let input = "ciao a tutti!";
+//console.log(palindromes(input));
 
 // Do not edit below this line
 module.exports = palindromes;
