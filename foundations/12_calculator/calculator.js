8aee9d96bbaf565eee01688ae846775bfd8f1b0f
martabalbo <marta99.balbo@gmail.com>
2026-06-04 17:55:50 +0200

    Add the calculator functions

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+	for (const el of arr) {
+    total = total + el;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  for (const el of arr) {
+    total = total * el;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  let total = 1;
+	for (let i = 0; i < exp; i++) {
+    total = total * base;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for (let i = 0; i < num; i++) {
+    total = total * (i+1);
+  }
+  return total;
 };
 
 // Do not edit below this line
